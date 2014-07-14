dce168d3a871b29c461cc289d4f87d5ca2e3591b
gmturbo <gmturbo@me.com>
2014-07-13 23:03:39 -0600

    init

---
+++ server.js
@@ -0,0 +1,88 @@
+
+//server for ANON_DIR
+
+//needs to have a object with the unique keys sent from the clients
+
+//****************** OBJS *******************
+
+
+var uid2sock = {
+  //uniq ID : session
+};
+
+// var session = function(id){
+//     this.id: id,
+//     this.socks: {} // {socket1.id: socket1, socket2.id:socket2}
+// };
+
+//****************** SERVER HTTP && socks ****************
+
+var pjson = require('./package.json')
+
+var socketio = require('socket.io');
+var io = socketio.listen(pjson.port);
+
+io.sockets.on('connection', function(socket){
+  
+  //outgoing
+  socket.emit('requestKey', {});
+  
+  // socket.on('fileDataRead', function(data){
+  //   console.log('fileDataRead event');
+  //   //data should have a unique id and 3 states [begin, sending, end]
+  //   uid2sock[data.key].forEach(function(sock){
+  //     if(sock.id !== socket.id)
+  //       sock.emit("fileDataRead", data);
+  //   });
+    
+  // });
+  
+  socket.on('fileDataWrite', function(data){
+    console.log('fileDataWrite event');
+    //data should have a unique id and 3 states [begin, sending, end]
+    uid2sock[data.key].forEach(function(sock){
+      if(sock.id !== socket.id)
+        sock.emit("fileDataRead", data);
+    });
+  });
+  
+  //should return a relative filename wrt to the
+  //synced file structure
+  //ex: /beep/boop.png
+  socket.on('fileSynced', function(data){
+    
+  });
+  
+  //incoming
+  socket.on('newKey', function(data){
+    console.log('newKey event');
+    if( uid2sock[data.key] !== undefined)
+      uid2sock[data.key].push(socket);
+    else{
+      uid2sock[data.key] = [];
+      uid2sock[data.key].push(socket);
+    }
+    
+    socket.emit('ready', {});
+    
+  })
+  
+  //incoming
+  socket.on('getUserCount', function(){
+    
+  });
+  
+  //incoming
+  socket.on('disconnect', function(){
+    
+  });
+  
+});
+
+
+
+
+console.log('server listening at ' + pjson.server + ":" + pjson.port)
+
+
+
---
+++ tmp/client.js
@@ -0,0 +1,47 @@
+var socketio = require('socket.io-client'),
+    args = require('minimist')(process.argv.slice(2));
+
+var pjson = require('./package.json'),
+    color = require('ansi-color').set;
+    
+var fs = require('fs'),
+    through = require('through'),
+    path = require('path');
+    
+var Mirror = require('./mirror');
+
+if(!(args.dir || args.d)){
+  console.log('you need to provide a monitor directoy --dir "{dir}"')
+}
+
+var port = args.port || pjson.port;
+var server = args.server || pjson.server;
+var fullServer = server + ':' + port;
+
+console.log(color('connecting to ' + fullServer, 'blue_bg'))
+
+var socket = socketio.connect('http://localhost:1337');
+
+socket.on('connect', function(data) {
+    console.log(color('successfully connected :)', 'cyan_bg'));
+});
+
+socket.on('ready', function(){
+  var mirror = new Mirror(args.key);
+  mirror.createReadStream(args.dir, socket);
+  mirror.createWriteStream(args.dir, socket);
+})
+
+socket.on('requestKey', function(data){
+  socket.emit('newKey', {key: args.key});
+});
+
+socket.on('disconnect', function(data){
+   console.log(color('disconnected :(','red_bg'));
+});
+
+socket.on('error', function(data){
+  console.dir(data);
+});
+
+
