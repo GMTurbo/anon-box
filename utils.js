00f9b911cb831e6029a57a3cdac715d1b8ebb2a7
gmturbo <gmturbo@me.com>
2014-07-16 21:51:27 -0600

    commented utils

--- utils.js
+++ utils.js
@@ -1,15 +1,21 @@
 //generic utility functions
 
-var path = require('path'),
-    color = require('ansi-color').set;
+var path = require('path'), // directory helper
+    color = require('ansi-color').set; //terminal colorer
 
+//export an object with all our functions
 module.exports = {
+  
+    //get a random color from a rolling list of them
     getBgColor: function() {
-
+        
+        //color coincide with supported colors in the ansi-color module
         var colors = ['red_bg', 'blue_bg', 'cyan_bg', 'magenta_bg', 'green_bg'];
 
         var roller = -1;
-
+        
+        //return the output of a function that increments roller
+        //the syntax below is just us creating a function and executing it immediately
         return (function() {
             var color = colors[roller];
             if (++roller > colors.length - 1)
@@ -17,6 +23,9 @@ module.exports = {
             return color;
         }());
     },
+    
+    //special console.log
+    //it overwrites the previous line so we can display speeds and stuff
     console_out: function(msg, overwrite) {
         process.stdout.clearLine();
         process.stdout.cursorTo(0);
@@ -25,24 +34,36 @@ module.exports = {
         }
         process.stdout.write(msg);
     },
+    
+    //return human readable representation of file sizes
+    //I didn't write this
     bytes: function(size) {
         if (!size) return '0 B';
         var i = Math.floor(Math.log(size) / Math.log(1024));
         return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
     },
+    
+    //print pretty text to terminal
     printPretty: function(message, clr, override) {
         this.console_out(color(message, clr ? clr : getBgColor()), override);
     },
+    
+    //ex:
+    // c:/gabe/is/awesome/and/cool
+    // c:/gabe/is/awesome/
+    // returns /and/cool/
     pathDif: function(path1, path2) {
-        var p1 = path1.split(path.sep);
-        var p2 = path2.split(path.sep);
+      
+        var p1 = path1.split(path.sep),
+            p2 = path2.split(path.sep);
+        
+        //removes any empty elements in the array returned from split
         p1 = p1.filter(function(n) {
             return (n !== '');
         });
         p2 = p2.filter(function(n) {
             return (n !== '');
         });
-        //var lng = (p1.length >= p2.length) ? p1.length : p2.length;
 
         var offset = p1.length - p2.length;
 
