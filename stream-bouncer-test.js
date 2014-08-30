6e3551e8faf7f039dfec6ce634337a41e9b6f51c
Garrett Dawson <killtheliterate@gmail.com>
2014-08-30 16:55:11 -0600

    Do some junk

--- stream-bouncer-test.js
+++ stream-bouncer-test.js
@@ -5,15 +5,14 @@ var sb = new StreamBouncer();
 
 for (var i = 1; i < 5; i += 1) {
   sb.push({
-    source: fs.createReadStream('~/Desktop/anonbox-target/master/' + i + '.jpg'),
-    destination: fs.createWriteStream('~/Desktop/anonbox-target/slave/' + i + '.jpg'),
+    source: fs.createReadStream('/Users/gdawson/Desktop/anonbox-target/master/' + i + '.jpg'),
+    destination: fs.createWriteStream('/Users/gdawson/Desktop/anonbox-target/slave/' + i + '.jpg'),
   });
-
 }
 
 for (var j = 1; j < 6; j += 1) {
   sb.push({
-    source: fs.createReadStream('~/Desktop/anonbox-target/master/' + j + '.jpg'),
-    destination: fs.createWriteStream('~/Desktop/anonbox-target/slave/' + j * 2 + '.jpg')
+    source: fs.createReadStream('/Users/gdawson/Desktop/anonbox-target/master/' + j + '.jpg'),
+    destination: fs.createWriteStream('/Users/gdawson/Desktop/anonbox-target/slave/' + j * 2 + '.jpg')
   });
 }
